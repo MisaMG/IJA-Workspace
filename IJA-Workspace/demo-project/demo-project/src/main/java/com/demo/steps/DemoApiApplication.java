aad67890cbd5d68990bfa16bf525b360ee4043d6
MisaMG <misael.mendoza@potros.itson.edu.mx>
2022-05-24 23:32:45 -0700

    step1 : creating our conroller

--- IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/DemoApiApplication.java
+++ IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/DemoApiApplication.java
@@ -1,10 +1,10 @@
-package com.demo.demoproject;
+package com.demo.steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
+
 public class DemoApiApplication {
 
 	public static void main(String[] args) {
--- IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/controller/TaskS1Controller.java
+++ IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/controller/TaskS1Controller.java
@@ -1,5 +1,12 @@
+
 package com.demo.steps.controller;
 
+import java.util.List;
+
+import com.demo.steps.entities.Task;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,34 +14,40 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/tasks")
-public class TaskS1Controller{
-    
-	@GetMapping("/hi")
-	public String sayHello(){
-		return "Hello World! again";
-	}
+public class TaskS1Controller {
 
+	// Response 200 ok Standard Response for successful HTTP request
 	@GetMapping("/all")
-	public String fetchAllTask(){
-		return "[] Returning all tasks";
+	public ResponseEntity<List<Task>> fetchAllTask() {
+		Task task = new Task();
+		task.setId(1L);
+		task.setTitle("Task1");
+		task.setDescription("Description of Task1");
+
+		return null; //ResponseEntity.ok("localhost:8080/tasks");
 	}
 
+	// Response 201 Created The response has been fulfilled and resulted in a new
+	// resource being created
 	@PostMapping("/save")
-	public String postTask(){
-		return "Tasks has been added";
+	public ResponseEntity<Task> postTask(Task newTask) {
+		//return ResponseEntity.status(HttpStatus.CREATED).body("Task Created");
+		return null;
 	}
 
+	// Response 204 No Content The server successfully processed the request, but
+	// it's not returning any content
 	@DeleteMapping("/delete")
-	public String deleteTask(){
-		return "Tasks has been deleted";
+	public ResponseEntity<Void> deleteTask(Long taskId) {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
+	// Response 200 ok
 	@PutMapping("/update")
-	public String putTask(){
-		return "Tasks has been updated";
+	public ResponseEntity<Task> putTask(Long taskId, Task updatedTask) {
+		return null; //ResponseEntity.ok().body("Task Updated");
 	}
 
 }
