9ad29145afc44f56710e6913471df300beb04d87
MisaMG <misael.mendoza@potros.itson.edu.mx>
2022-05-25 01:07:26 -0700

    step1-1 : updating our controller

--- IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/controller/TaskS1Controller.java
+++ IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/controller/TaskS1Controller.java
@@ -1,6 +1,8 @@
 
 package com.demo.steps.controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.demo.steps.entities.Task;
@@ -9,8 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,36 +22,67 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tasks")
 public class TaskS1Controller {
 
+	private final List<Task> taskList = new ArrayList<>();
+
 	// Response 200 ok Standard Response for successful HTTP request
 	@GetMapping("/all")
 	public ResponseEntity<List<Task>> fetchAllTask() {
-		Task task = new Task();
-		task.setId(1L);
-		task.setTitle("Task1");
-		task.setDescription("Description of Task1");
-
-		return null; //ResponseEntity.ok("localhost:8080/tasks");
+		return ResponseEntity.ok().body(taskList);
 	}
 
 	// Response 201 Created The response has been fulfilled and resulted in a new
 	// resource being created
 	@PostMapping("/save")
 	public ResponseEntity<Task> postTask(Task newTask) {
-		//return ResponseEntity.status(HttpStatus.CREATED).body("Task Created");
-		return null;
+		newTask.setActive(false);
+		newTask.setCreatedAt(LocalDateTime.now());
+		long id = taskList.size() + 1;
+		newTask.setId(id);
+		taskList.add(newTask);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
 	}
+
+	@GetMapping("/{taskId}")
+	public ResponseEntity<Task> fetchTaskById( @PathVariable("taskId") Long taskId) {
+		return taskList
+			.stream()
+			.filter( current -> taskId == current.getId() )
+			.findFirst()
+			.map( task -> ResponseEntity.ok().body(task) )
+			.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+	}
+
 
 	// Response 204 No Content The server successfully processed the request, but
 	// it's not returning any content
-	@DeleteMapping("/delete")
-	public ResponseEntity<Void> deleteTask(Long taskId) {
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	@DeleteMapping("/{taskId}")
+	public ResponseEntity<?> deleteTask(@PathVariable("taskId")  Long taskId) {
+		return taskList
+					.stream()
+					.filter( current -> taskId == current.getId())
+					.findFirst()
+					.map(task ->{
+						taskList.remove(task);
+						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+					})
+					.orElse(ResponseEntity.status(HttpStatus.NO_CONTENT).build());
 	}
 
 	// Response 200 ok
-	@PutMapping("/update")
-	public ResponseEntity<Task> putTask(Long taskId, Task updatedTask) {
-		return null; //ResponseEntity.ok().body("Task Updated");
+	@PutMapping("/{taskId}")
+	public ResponseEntity<Task> putTask(@PathVariable("taskId") Long taskId, Task updatedTask) {
+		return taskList
+					.stream()
+					.filter( current -> taskId == current.getId())
+					.findFirst()
+					.map( task ->{
+						task.setTitle( updatedTask.getTitle());
+						task.setDescription(updatedTask.getDescription());
+						task.setUpdatedAt(LocalDateTime.now());
+						return ResponseEntity.ok().body(task);
+					})
+					.orElse(ResponseEntity.status(HttpStatus.NO_CONTENT).build());
 	}
 
 }
--- IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/entities/Task.java
+++ IJA-Workspace/demo-project/demo-project/src/main/java/com/demo/steps/entities/Task.java
@@ -1,10 +1,15 @@
 package com.demo.steps.entities;
 
+import java.time.LocalDateTime;
+
 public class Task {
 
     private long id;
     private String title;
     private String description;
+    private Boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public long getId() {
         return id;
@@ -30,4 +35,28 @@ public class Task {
         this.description = description;
     }
 
+    
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
